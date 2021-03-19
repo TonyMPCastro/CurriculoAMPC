6e5a47cffe75492d76b6c24f918e6ac4b97b94c4
Antonio Marcos Patricio Castro <54753638+TonyMPCastro@users.noreply.github.com>
2021-03-19 19:22:21 -0300

    a

--- src/component/Segundo.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-//Função com Parametros
-    export default  props =>
-    <>
-        <h1 id='t'>{props.titulo}</h1>  
-        <h2 id='tt'>{props.subtitulo}</h2> 
-    </>   
--- src/data/produtos.js
+++
@@ -1,9 +0,0 @@
-
-export default [
-    {id:1, nome: 'Caneta', preco: 7.5},
-    {id:2, nome: 'Lapis', preco: 3.5},
-    {id:3, nome: 'Coretivo', preco: 5.5},
-    {id:4, nome: 'Caderno', preco: 20.5},
-
-
-]
